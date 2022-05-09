9ae892fb8886824b1d15e2e69ba6e7cc589437cf
Azat Allayarov <azatallayarow@gmail.com>
2022-05-09 17:03:58 +0900

    submit

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,26 @@
+require 'open-uri'
+class GamesController < ApplicationController
+  def new
+    @letters = ('A'..'Z').to_a.sample(10)
+  end
+
+  def score
+    @word_array = params[:word].upcase.split
+    @word = params[:word].upcase
+    url = "https://wagon-dictionary.herokuapp.com/#{@word}"
+    word_searched = URI.open(url).read
+    @result = JSON.parse(word_searched)
+    @current_letters = params[:letters].split
+    @ingrid = @word.chars.all? do |letter|
+      @word.count(letter) <= @current_letters.count(letter)
+    end
+
+    if @ingrid && @result["found"]
+      @results = "Congratulations! #{params[:word]} is a valid English word!"
+    elsif @ingrid && @result["found"] == false
+      @results = "Sorry but #{params[:word]} does not seem to be English word..."
+    else
+      @results = "Sorry but #{params[:word]} cannot be build out of #{@letters}"
+    end
+  end
+end
